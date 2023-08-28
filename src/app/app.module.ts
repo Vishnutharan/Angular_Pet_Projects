9f6b196794b19c11e0e7e2f092ee61bc63ea07a2
Vishnu <karunakaran.vishnu6@gmail.com>
2023-08-28 18:22:35 +0530

    PetProject calculater

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { CalculaterComponent } from './calculater/calculater.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    CalculaterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/calculater/calculater.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculater',
+  templateUrl: './calculater.component.html',
+  styleUrls: ['./calculater.component.css']
+})
+export class CalculaterComponent {
+
+}
